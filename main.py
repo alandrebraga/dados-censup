544507cc859ce9ac4a1af2e69bf1533e23ea320e
ANDRE LUIZ BRAGA MALLMANN <andre.mallmann@adm.univates>
2023-12-05 14:44:12 -0300

    refactor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from source.extract import Downloader
 from source.load import run
+from source.load import convert_and_delete_files
 
 if __name__ == "__main__":
-    Downloader.download_files()
+    # Downloader.download_files()
     run()
--- source/load.py
+++ source/load.py
@@ -1,13 +1,16 @@
 from source.utils import find_file_by_suffix, convert_csv_to_parquet, delete_csv_files
 import pyarrow.parquet as pq
-from sqlalchemy import create_engine, text
-import polars as pl
+from psycopg2.extensions import register_adapter, AsIs, Float
+from sqlalchemy import create_engine, text, MetaData, Table
+import pandas as pd
+import numpy as np
 
-URI = "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+
+URI = "postgresql+psycopg2://bi_academico:bi_academico@biunivates.univates.br:5432/censu_educacao"
 
 
 def convert_and_delete_files():
-    csv_list = find_file_by_suffix("data", "csv")
+    csv_list = find_file_by_suffix("data")
     convert_csv_to_parquet(csv_list)
     delete_csv_files(csv_list)
 
@@ -23,8 +26,7 @@ def generate_schema(file_list: list) -> list:
 
 
 def run_sql(sql: str):
-    engine = create_engine(URI)
-
+    engine = create_engine(URI, execution_options={'executemany_mode': 'values'})
     connection = engine.connect()
     connection.execute(text(sql))
     connection.commit()
@@ -32,13 +34,30 @@ def run_sql(sql: str):
 
 
 def insert_values(file: str, table_name: str):
-    print(f"data/{file}")
-    df = pl.read_parquet(f"data/{file}")
+    engine = create_engine(URI, execution_options={'executemany_mode': 'values'})
+    df = pd.read_parquet(f"data/{file}")
 
     df.columns = list(map(lambda x: x.lower(), df.columns))
 
     try:
-        df.write_database(table_name=table_name, if_exists="append", connection=URI)
+        metadata = MetaData()
+        table = Table(table_name, metadata,autoload_with=engine)
+
+        with engine.connect() as conn:
+            table_columns = [col.name for col in table.columns]
+            df_columns = [col for col in df.columns if col in table_columns]
+            df = df[df_columns]
+
+            df = df.fillna(AsIs('NULL'))
+
+            int_columns = [col for col in df.columns if df[col].dtype == 'float64']
+            df[int_columns] = df[int_columns].astype('Int64')
+
+            conn.execute(table.insert(), df.to_dict(orient="records"))
+
+            conn.commit()
+            conn.close()
+
     except Exception as e:
         print(f"Erro ao inserir dados na tabela {table_name}: {e}")
 
@@ -69,17 +88,19 @@ def create_table_from_schema(schema: list, table_name) -> list:
 
 
 def run():
+    convert_and_delete_files()
     files = find_file_by_suffix("data", "parquet")
     cursos = [file for file in files if "CURSOS" in file]
     ies = [file for file in files if "IES" in file]
 
+    print(ies)
     schema_curso = generate_schema(cursos)
 
     schema_ies = generate_schema(ies)
 
     create_table_from_schema(schema_curso, "cursos")
 
-    ies = create_table_from_schema(schema_ies, "ies")
+    create_table_from_schema(schema_ies, "ies")
 
     for file in cursos:
         insert_values(file, "cursos")
